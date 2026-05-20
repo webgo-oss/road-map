f333d44146160eb85c20c1b8a393cab95905cc4a
Hamza <webgo235@gmail.com>
2026-05-20 20:23:38 +0530

    small chnage

--- node_frontend/app.js
+++ node_frontend/app.js
@@ -34,13 +34,13 @@ app.use((req, res, next) => {
   res.locals.user = req.session.user || null;
   next();
 });
-
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "ai_topic_app",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
 db.connect((err) => {
   if (err) throw err;
   console.log("✅ MySQL connected!");
